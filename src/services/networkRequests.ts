5a583f8435ddc9cda180c098bc6067e416e530bf
antoniovuono <antoniovuono@brq.com>
2023-05-21 17:43:52 -0300

    feat: generics added to the project

--- src/services/networkRequests.ts
+++ src/services/networkRequests.ts
@@ -1,10 +1,15 @@
 import { isAxiosError } from "axios";
 
 import { createInstance } from "./api";
+import { IStocksRequest } from "./interface";
+
+export interface HttpResponse<T> {
+  data: T[];
+}
 
 const getRequest = (route: string, token?: string) => {
   try {
-    const response = createInstance.get(route, {
+    const response = createInstance.get<HttpResponse<IStocksRequest>>(route, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
