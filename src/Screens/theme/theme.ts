a91d2173ab5667ae50d62620635a8ad70e34de53
antoniovuono <antoniovuono@brq.com>
2023-05-24 20:00:13 -0300

    feat: style theme

--- src/Screens/theme/theme.ts
+++ src/Screens/theme/theme.ts
@@ -1,17 +1,17 @@
 export default {
-    colors: {
-        PRIMARY_BACKGROUND: '#120423',
-        SECONDARY_BACKGROUND: '#1B0732',
-        PRIMARY_TITLE: '#FFFFFF',
-        SECONDARY_TITLE: '#F4D35E',
-        SUCCESS: '#37FF8B',
-        WARNING: "#FF5A5F",
-        BORDERS: '#292929'
-    }, 
-    fonts: {
-        PRIMARY_REGULAR: 'Poppins_400Regular',
-        PRIMARY_MEDIUM: 'Poppins_500Medium',
-        PRIMARY_SEMIBOLD: 'Poppins_600SemiBold',
-        PRIMARY_BOLD: 'Poppins_700Bold',
-    }
-}
+  colors: {
+    PRIMARY_BACKGROUND: "#120423",
+    SECONDARY_BACKGROUND: "#1B0732",
+    PRIMARY_TITLE: "#FFFFFF",
+    SECONDARY_TITLE: "#F4D35E",
+    SUCCESS: "#37FF8B",
+    WARNING: "#FF5A5F",
+    BORDERS: "#292929",
+  },
+  fonts: {
+    PRIMARY_REGULAR: "Poppins_400Regular",
+    PRIMARY_MEDIUM: "Poppins_500Medium",
+    PRIMARY_SEMIBOLD: "Poppins_600SemiBold",
+    PRIMARY_BOLD: "Poppins_700Bold",
+  },
+};
