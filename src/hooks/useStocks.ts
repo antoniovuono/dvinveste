c5d0c991cfa18f423aa8d08af153ab222684f57e
antoniovuono <antoniovuono@brq.com>
2023-06-03 21:22:35 -0300

    adjusting stock load more items

--- src/hooks/useStocks.ts
+++ src/hooks/useStocks.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { getRequest } from "../services/networkRequests";
 import { useStocksStore } from "../stores/useStocksStore";
